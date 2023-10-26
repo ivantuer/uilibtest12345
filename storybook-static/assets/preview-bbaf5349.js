3ca69ed18d87d0b01815ad546ea49020eac134a6
ivantuer <ivan.hrynevytskyi@Ivans-MacBook-Pro.local>
2023-10-26 12:15:08 +0300

    added storybook with gh-actions

---
+++ storybook-static/assets/preview-bbaf5349.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-4afd1b73.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-0943e212.js"),["./DocsRenderer-NNNQARDV-0943e212.js","./iframe-4afd1b73.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-cad8c1b2.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-bbaf5349.js.map
